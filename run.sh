b938a13a67fda9faeb84e7983f258a74349d1df9
longnx <long.nguyenxuan@outlook.com>
2020-02-08 00:09:59 +0700

    add aliases

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # tạo symlink cho vim 
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
-
 # tạo symlink cho bash 
 ln -sf ~/dotfiles/bash/bashrc ~/.bashrc
-
+ln -sf ~/dotfiles/bash/bash_aliases ~/.bash_aliases
